27e66c5d09e547bc5d0cca13f3a95f687ab1b6cf
Jakub <kuba.xray@gmail.com>
2024-05-07 15:17:50 +0200

    added footer content

--- redantenna/src/Components/footer/footer.jsx
+++ redantenna/src/Components/footer/footer.jsx
@@ -1,5 +1,47 @@
 import { FooterStyled } from ".";
+import { useContext } from "react";
+import { LanguageContext } from "../languageChange/languageContext";
 
 export const Footer = () => {
-	return <FooterStyled></FooterStyled>;
+	const { languageData } = useContext(LanguageContext);
+	const footer = languageData.footer;
+	return (
+		<FooterStyled>
+			<ul>
+				<li>
+					<h5 className='footerTextColor'>{footer.titleAbout}</h5>
+					<p className='footerTextColor'>{footer.about}</p>
+				</li>
+				<li>
+					<h5 className='footerTextColor'>{footer.titleContact}</h5>
+					<div>
+						<img src='' alt='' />
+						<p className='footerTextColor'>{footer.phoneMain}</p>
+					</div>
+					<div>
+						<img src='' alt='' />
+						<p className='footerTextColor'>{footer.phoneSec}</p>
+					</div>
+					<div>
+						<img src='' alt='' />
+						<p className='footerTextColor'>{footer.email}</p>
+					</div>
+				</li>
+				<li>
+					<h5 className='footerTextColor'>{footer.titleStore}</h5>
+					<p className='footerTextColor'>{footer.ourStore}</p>
+					<div>
+						<p className='footerTextColor'>{footer.open}</p>
+						<p className='footerTextColor'>{footer.hours}</p>
+					</div>
+					<p className='footerTextColor'>{footer.street}</p>
+					<div>
+						<p className='footerTextColor'>{footer.city}</p>
+						<p className='footerTextColor'>{footer.postCode}</p>
+					</div>
+					<p className='footerTextColor'>{footer.country}</p>
+				</li>
+			</ul>
+		</FooterStyled>
+	);
 };
--- redantenna/src/Components/footer/index.jsx
+++ redantenna/src/Components/footer/index.jsx
@@ -5,4 +5,13 @@ export const FooterStyled = styled.footer`
 	width: 100%;
 	height: 20rem;
 	background-color: ${COLORS.darkPurple};
+	color: ${COLORS.White};
+
+	.footerTextColor {
+		color: ${COLORS.White};
+	}
+
+	ul {
+		display: flex;
+	}
 `;
